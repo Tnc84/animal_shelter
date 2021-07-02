c7b495e6034f9973063f85c1f48fea60a28d4b31
TncBogdan <tncbogdan@gmail.com>
2021-07-02 22:54:43 +0300

    add entity and repository for animal

---
+++ src/main/java/com/tnc/repository/interfaces/AnimalRepository.java
@@ -0,0 +1,9 @@
+package com.tnc.repository.interfaces;
+
+import com.tnc.repository.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
